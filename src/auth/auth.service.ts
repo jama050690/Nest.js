db9c819d52cbc14ed2f5c052a12c9f6e7437d55e
Jamshiddin Babajonov <jbm050690@hotmail.com>
2026-06-11 18:17:04 +0500

    Nestda login va guarddan foydalanildi?

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,18 @@
+
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserService } from '../user/user.service';
+
+@Injectable()
+export class AuthService {
+    constructor(private readonly usersService: UserService) { }
+
+    async signIn(username: string, pass: string): Promise<any> {
+        const user = await this.usersService.findOne(username);
+        if (user?.password !== pass) {
+            throw new UnauthorizedException();
+        }
+        const { password, ...result } = user;
+
+        return result;
+    }
+}
---
+++ src/auth/constants.ts
@@ -0,0 +1,5 @@
+export const jwtConstants = {
+    secret: 'DO_NOT_USE_THIS_IN_PRODUCTION_USE_ENV_VARIABLE',
+};
+
+export const IS_PUBLIC_KEY = 'isPublic';
